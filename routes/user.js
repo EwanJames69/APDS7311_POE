c08cb19c43c2cdb2804e93b0823f293b14681adf
EwanJames69 <ewanjames52@gmail.com>
2023-11-05 19:27:50 +0200

    Minor changes to backend

--- routes/user.js
+++ routes/user.js
@@ -64,37 +64,3 @@ router.post('/login', bruteforce.prevent, (req, res) => {
 })
 
 module.exports = router
-
-/*
-const router = require('express').Router();
-const auth = require('../middleware/auth');
-const { hashPassword } = require('../utils/hash');
-
-// Create new user
-router.post('/signup', async (req, res) => {
-    const { error } = validateUser(req.body);
-    if (error) return res.status(400).json(error.details[0].message);
-
-    const isUnique = (await User.count({ username: req.body.username })) === 0;
-    if (!isUnique){
-        return res.status(400).json({ error: 'The username or password is not valid'});
-    }
-
-    try {
-        const user = new User(req.body);
-        user.password = await hashPassword(user.password);
-        await user.save();
-    } catch (err) {
-        return res.status(500).json(err);
-    }
-    res.sendStatus(201);
-});
-
-/*
-// Get current user details
-router.get('/', auth, async (req, res) => {
-    res.send({ currentUser: req.user});
-});
-
-module.exports = router;
-*/
